ec69b82f4e6fb7cc814470ff061848a0ce4e2078
刘忍 <18826073587@163.com>
2016-07-07 16:56:27 +0800

    修改 gulpfile
    添加用 gulp 将 ls 编译为 js 的模块；添加又去掉了压缩图片模块，因为太慢；进行了测试

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,13 +1,37 @@
 var gulp = require('gulp');
+
+var imageMin = require('gulp-imagemin');
+
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
-// express app 启动
+var gulpLiveScript = require('gulp-livescript');
+var uglify = require('gulp-uglify');
+
 var express = require('express');
 var app = express();
 
-gulp.task('express', ['sass'], function() {
+// 将 sass 编译为 min.css
+gulp.task('sass', function () {
+  return gulp.src('src/sass/*.sass')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('static/css'));
+});
+
+// 将 ls 编译为 min.js
+gulp.task('ls', ['sass'], function() {
+  return gulp.src('src/ls/*.ls')
+    .pipe(gulpLiveScript({bare: true}))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('static/js'));
+});
+
+// express app 启动
+gulp.task('express', ['ls'], function() {
   app.get('/', function (req, res) {
     res.send('Hello World!');
   });
@@ -15,15 +39,6 @@ gulp.task('express', ['sass'], function() {
   app.listen(3000, function () {
     console.log('Example app listening on port 3000');
   });
-});
-
-// 将 sass 编译为 min.css
-gulp.task('sass', function () {
-  return gulp.src('static/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('static/css'));
 });
 
 gulp.task('default', ['express'], function() {
