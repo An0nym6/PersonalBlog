adb2b3f9402929eea5c4f1535a021fed769bcb6f
刘忍 <18826073587@163.com>
2016-07-14 17:01:53 +0800

    完成时间轴部分
    完成时间轴从服务器读取数据并加载成框

--- website/index.js
+++ website/index.js
@@ -5,4 +5,15 @@ router.get('/', function(req, res, next) {
   res.render('layout', { title: "RANDOM THOUGHTS | 刘忍的个人博客" });
 });
 
+var timeline = [{icon: 'travel', imgUrl: 'img/timeline/timeline1.png', title: '剑桥之旅',
+                 details: '悄悄的我走了，正如我悄悄的来；我挥一挥衣袖，不带走一片云彩...',
+                 time: '2015年8月', likes: 12},
+                {icon: 'study', imgUrl: 'img/timeline/timeline2.png', title: '中山大学',
+                 details: '白云山高，珠江水长，吾校矗立，蔚为国光， 中山手创，遗泽余芳...',
+                 time: '2014年8月', likes: 31}];
+
+router.get('/timeline', function(req, res, next) {
+  res.json(timeline);
+});
+
 module.exports = router;
--- website/src/ls/timeline.ls
+++ website/src/ls/timeline.ls
@@ -1,5 +1,12 @@
 angular.module 'timeline' ['ngMaterial']
 
-.controller 'timelineController' !->
+.controller 'timelineController' ($http) !->
   @timelineHeight = 2048
-  
+  that = @
+
+  # 请求时间轴的数据
+  $http { method: 'GET', url: '/timeline' }
+  .then (response) !->
+    that.timeline = response.data
+  , (response) !->
+    console.log response
